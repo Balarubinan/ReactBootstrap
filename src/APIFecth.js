1a3450917fdadd97409f6b7520898d7cf8768788
Bala <sudhagouthirubi@gmail.com>
2021-11-18 10:53:09 +0530

    *added all coins list *added scrollbar

--- src/APIFecth.js
+++ src/APIFecth.js
@@ -1,32 +1,27 @@
 var axios = require('axios');
 var fs=require('fs')
+
 let coinList=`BTC,ETH,BNB,LTC,QTUM,ADA,XRP,EOS,IO
 TA,XLM,TRX,ETC,VET,LINK,BTT,ZRX,BA
 T,XMR,ZEC,DASH,ENJ,MATIC,ATOM,ALGOI
-NR,DOGE,CHZ,BUSD,XTZ,BCH,FTT,SOL,KN
+NR,DOGE,CHZ,BUSD,XTZ,BCH,SOL,KN
 C,COMP,SNX,MKR,MANA,YFI,BAL,CRV,
 DOT,PAXG,SUSHI,UNI,AAVE,FIL,GRT,1INC
 H,CAKE,ICP,USDP`
 
-// let coinList="BTC,ETH,BNB"
+// let coinList="BTC,ETH,BNB,GRT,DOGE,FIL,ICP"
 
 export async function fetchCoinData(callBack){
 
     var config = {
         method: 'get',
         url: `https://api.nomics.com/v1/currencies/ticker?key=1f817e98d914351cb28cf89769a40b356d99ddc6&ids=${coinList}&interval=1h`,
-        // headers: {
-        //   'Access-Control-Allow-Origin':'*',
-        //   'Access-Control-Allow-Methods':'GET, POST, PUT'
-        // }
       };
       let dataObj=null
       await axios(config)
       .then(function (response) {
         dataObj=response.data
-        console.log("Response arrived"+response.data)
         callBack(response.data)
-        console.log("After call to calback")
       })
       .catch(function (error) {
         console.log(error);
--- src/ListComp.js
+++ src/ListComp.js
@@ -1,19 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle'
-// import staticFetch from './APIFecth';
 import fetchCoinData from './APIFecth'
 import {BsFillArrowDownCircleFill,BsFillArrowUpCircleFill} from "react-icons/bs"
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
+
+var count=0
 function ListComp(props){
-    const [coins, setcoins] = useState([])
+    const [coins, setcoins] = useState([])  
     function fetchCallback(res){
         setcoins(res)
-        console.log("From callback"+res)
+        console.log("From callback :"+count++)
     }
-    setInterval(()=>fetchCoinData(fetchCallback),10000)
+    // fetchCoinData(fetchCallback)
+    // setInterval(()=>fetchCoinData(fetchCallback),11000) // api refreshes data only after 10 secs.....so fetching after 11 secs
+    useEffect(() => {
+        fetchCoinData(fetchCallback)
+        let intervalId=setInterval(()=>fetchCoinData(fetchCallback),11000)
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
     let listTitle=props.title
     return(
-    <div className="col-md-6 col-xl-6">
+    <div className="col-md-6 col-xl-6" style={{'overflow-y':'scroll'}}>
         <div className="card shadow mb-4" style={{opacity: 1,filter: "blur(0px)",height: "380px",backgroundColor: "rgba(255,255,255,0)",color: "rgb(255,255,255)"}}>
             <div className="col">
                 {listTitle}
@@ -45,10 +54,10 @@ function ListComp(props){
                         </div>
                         <div className="row">
                             <div className="col">
-                                {parseFloat(v['1h'].price_change_pct)>0&&
-                                <h4 className="small font-weight-bold" style={{color:"green"}}><BsFillArrowUpCircleFill color="green" size="20"/>  <span className="float-left">{parseFloat(v['1h'].price_change_pct).toFixed(2)+"%"}</span></h4>}
-                                {parseFloat(v['1h'].price_change_pct)<0&&
-                                <h4 className="small font-weight-bold" style={{color:"red"}}><BsFillArrowDownCircleFill color="red" size="20"/>  <span className="float-left">{parseFloat(v['1h'].price_change_pct).toFixed(2)+"%"}</span></h4>}
+                                {parseFloat(v['1h'].price_change)>0&&
+                                <h4 className="small font-weight-bold" style={{color:"green"}}><BsFillArrowUpCircleFill color="green" size="20"/>  <span className="float-left">{parseFloat(v['1h'].price_change).toFixed(2)+"%"}</span></h4>}
+                                {parseFloat(v['1h'].price_change)<0&&
+                                <h4 className="small font-weight-bold" style={{color:"red"}}><BsFillArrowDownCircleFill color="red" size="20"/>  <span className="float-left">{parseFloat(v['1h'].price_change).toFixed(2)+"%"}</span></h4>}
                             </div>
                         </div>
                     </div>
